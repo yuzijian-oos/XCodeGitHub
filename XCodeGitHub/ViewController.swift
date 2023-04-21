4e9a602b3f0ab487b2ebe3a0bc970a2d99ea260e
余子健 <1441942979@qq.com>
2023-04-21 18:10:35 +0800

    ViewController修改

--- XCodeGitHub/ViewController.swift
+++ XCodeGitHub/ViewController.swift
@@ -17,8 +17,17 @@ class ViewController: UIViewController {
         view1.backgroundColor = .red
         view1.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
         view.addSubview(view1)
+        
+        let textfield = UITextView()
+        textfield.layer.borderColor = UIColor.black.cgColor
+        textfield.layer.borderWidth = 1
+        textfield.frame = CGRect(x: 50, y: 150, width: 200, height: 300)
+        view.addSubview(textfield)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
 }
 
